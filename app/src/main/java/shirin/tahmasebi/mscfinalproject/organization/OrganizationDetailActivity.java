95e08756fa98cdd3fb06a676e0168cd6317559e8
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2016-08-13 01:52:31 +0430

    ظاهر لیست سازمان ها و صفحه نمایش جزییات را شروع کن

--- app/src/main/java/shirin/tahmasebi/mscfinalproject/organization/OrganizationDetailActivity.java
+++ app/src/main/java/shirin/tahmasebi/mscfinalproject/organization/OrganizationDetailActivity.java
@@ -1,16 +1,24 @@
 package shirin.tahmasebi.mscfinalproject.organization;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 
 import shirin.tahmasebi.mscfinalproject.MainActivity;
 import shirin.tahmasebi.mscfinalproject.R;
 
 public class OrganizationDetailActivity extends MainActivity {
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        mCollapsingToolbarLayout.setTitle(getString(R.string.title_activity_organizationDetail));
+        mCollapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+        final Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/IRAN-Sans-Bold.ttf");
+        mCollapsingToolbarLayout.setCollapsedTitleTypeface(tf);
+        mCollapsingToolbarLayout.setExpandedTitleTypeface(tf);
     }
 
     @Override
