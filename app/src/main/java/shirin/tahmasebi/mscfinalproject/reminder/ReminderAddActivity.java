3800576c5eb65cc652a7b1dce50b3f05df648d77
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2016-09-17 04:38:41 +0430

    ویوی یادآور را بهبود بده - انتخاب تایم و دیت را اضافه کن

--- app/src/main/java/shirin/tahmasebi/mscfinalproject/reminder/ReminderAddActivity.java
+++ app/src/main/java/shirin/tahmasebi/mscfinalproject/reminder/ReminderAddActivity.java
@@ -1,21 +1,35 @@
 package shirin.tahmasebi.mscfinalproject.reminder;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.mohamadamin.persianmaterialdatetimepicker.date.DatePickerDialog;
+import com.mohamadamin.persianmaterialdatetimepicker.time.RadialPickerLayout;
+import com.mohamadamin.persianmaterialdatetimepicker.time.TimePickerDialog;
+import com.mohamadamin.persianmaterialdatetimepicker.utils.PersianCalendar;
 
 import java.util.List;
 
 import shirin.tahmasebi.mscfinalproject.MainActivity;
 import shirin.tahmasebi.mscfinalproject.R;
 import shirin.tahmasebi.mscfinalproject.organization.SpinnerAdapter;
+import shirin.tahmasebi.mscfinalproject.util.Helper;
+import shirin.tahmasebi.mscfinalproject.view.FontableTextView;
 
 public class ReminderAddActivity extends MainActivity implements
-        ReminderAddPresenter.ReminderAddView {
+        ReminderAddPresenter.ReminderAddView,
+        TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener {
 
     private ReminderAddPresenter mPresenter;
+    private Spinner spinner;
+    private static final String TIMEPICKER = "TimePickerDialog",
+            DATEPICKER = "DatePickerDialog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +56,52 @@ public class ReminderAddActivity extends MainActivity implements
     @Override
     public void init(List<String> organizationName) {
         initSpinner(organizationName);
+        initTimePicker();
+        initDatePicker();
+    }
+
+    private void initDatePicker() {
+        PersianCalendar now = new PersianCalendar();
+        final DatePickerDialog dpd = DatePickerDialog.newInstance(
+                this,
+                now.getPersianYear(),
+                now.getPersianMonth(),
+                now.getPersianDay()
+        );
+        findViewById(R.id.reminder_date_textView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dpd.show(getFragmentManager(), DATEPICKER);
+            }
+        });
+    }
+
+    private void initTimePicker() {
+        PersianCalendar now = new PersianCalendar();
+        final TimePickerDialog tpd = TimePickerDialog.newInstance(
+                this,
+                now.get(PersianCalendar.HOUR_OF_DAY),
+                now.get(PersianCalendar.MINUTE),
+                true
+        );
+        tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                Log.d(TIMEPICKER, "Dialog was cancelled");
+            }
+        });
+        findViewById(R.id.reminder_time_textView).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        tpd.show(getFragmentManager(), TIMEPICKER);
+                    }
+                }
+        );
     }
 
     private void initSpinner(final List<String> list) {
-        Spinner spinner = (Spinner) findViewById(R.id.reminder_organizationName_spinner);
+        spinner = (Spinner) findViewById(R.id.reminder_organizationName_spinner);
         SpinnerAdapter adapter = new SpinnerAdapter(
                 this,
                 R.layout.item_spinner,
@@ -57,7 +113,7 @@ public class ReminderAddActivity extends MainActivity implements
             public void onItemSelected(AdapterView<?> adapterView,
                                        View view, int position, long id) {
                 Toast.makeText(ReminderAddActivity.this,
-                        list.get(position) + "", Toast.LENGTH_LONG).show();
+                        spinner.getSelectedItem().toString() + "", Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -65,5 +121,22 @@ public class ReminderAddActivity extends MainActivity implements
 
             }
         });
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        String hourString = hourOfDay < 10 ? "0" + hourOfDay : "" + hourOfDay;
+        String minuteString = minute < 10 ? "0" + minute : "" + minute;
+        String time = hourString + ":" + minuteString;
+        ((FontableTextView) findViewById(R.id.reminder_time_textView)).setText(
+                Helper.convertToPersianDigits(time));
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        // Note: monthOfYear is 0-indexed
+        String date = year + "/" + (monthOfYear + 1) + "/" + dayOfMonth;
+        ((FontableTextView) findViewById(R.id.reminder_date_textView)).setText(
+                Helper.convertToPersianDigits(date));
     }
 }
