b1362d588f0eff6c5dd2a16eb00a0a17a5b6b1b4
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2016-11-27 04:32:11 +0330

    سایز فایل اجرایی برنامه و مصرف حافظه را بهبود بده - یک

--- greendao-gen/src/main/java/shirin/tahmasebi/DbGenerator.java
+++ greendao-gen/src/main/java/shirin/tahmasebi/DbGenerator.java
@@ -2,12 +2,13 @@ package shirin.tahmasebi;
 
 import de.greenrobot.daogenerator.DaoGenerator;
 import de.greenrobot.daogenerator.Schema;
+
 import shirin.tahmasebi.schema.SchemaV2;
 
 public class DbGenerator {
     public static void main(String[] args) throws Exception {
 
-        Schema schemaV2 = new SchemaV2();
-        new DaoGenerator().generateAll(schemaV2, "./app/src/main/java/");
+        Schema schemaV1 = new SchemaV2();
+        new DaoGenerator().generateAll(schemaV1, "./app/src/main/java/");
     }
 }
